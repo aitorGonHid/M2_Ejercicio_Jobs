f4d62159009390b4da00969110575df273248c20
Adrián Pobo <adrianpoboadell@gmail.com>
2021-07-18 23:47:17 +0200

    elimino la interface
    ya esta todo pero la interface sobra

--- Clases/BonusSueldo.java
+++
@@ -1,8 +0,0 @@
-package Clases;
-
-public interface BonusSueldo {
-	// Calculo bonus sueldo
-	// Se implementa en Person
-	// Todas las clases implementan la interfaz menos Volunteer
-	public double getBonus();
-}
